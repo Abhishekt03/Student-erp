254828df7aa09cdaa2a70477e3f06acf8d4fe7ae
Abhishekktingani <148617285+Abhishekktingani@users.noreply.github.com>
2026-02-10 11:55:13 +0530

    Update after repo rename

--- src/main/java/com/erp/student_erp/entity/Marks.java
+++ src/main/java/com/erp/student_erp/entity/Marks.java
@@ -10,65 +10,78 @@ import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "marks")
-
 public class Marks {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "student_id", nullable = false)
-    private User student;
+	@ManyToOne
+	@JoinColumn(name = "student_id")
+	private User student;
 
-    @ManyToOne
-    @JoinColumn(name = "course_id", nullable = false)
-    private Course course;
+	@ManyToOne
+	@JoinColumn(name = "course_id", nullable = false)
+	private Course course;
 
-    public Long getId() {
+	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public User getStudent() {
 		return student;
 	}
+
 	public void setStudent(User student) {
 		this.student = student;
 	}
+
 	public Course getCourse() {
 		return course;
 	}
+
 	public void setCourse(Course course) {
 		this.course = course;
 	}
+
 	public int getInternal() {
 		return internal;
 	}
+
 	public void setInternal(int internal) {
 		this.internal = internal;
 	}
+
 	public int getExternal() {
 		return external;
 	}
+
 	public void setExternal(int external) {
 		this.external = external;
 	}
+
 	public int getTotal() {
 		return total;
 	}
+
 	public void setTotal(int total) {
 		this.total = total;
 	}
+
 	public String getGrade() {
 		return grade;
 	}
+
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
+
 	private int internal;
-    private int external;
-    private int total;
-    private String grade;
+	private int external;
+	private int total;
+	private String grade;
 }
